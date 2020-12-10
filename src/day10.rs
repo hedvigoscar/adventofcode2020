dba1c934a4c1fc6c0ff510d14bccbf2e210d7159
Oscar Nylander <oscar@hedvig.com>
2020-12-11 00:46:42 +0100

    Solve day 10, part 2.
    First one to completely stump me!

--- src/day10.rs
+++ src/day10.rs
@@ -3,26 +3,64 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 #[aoc_generator(day10)]
-fn parse_day10(input: &str) -> Vec<u16> {
-    input.lines().map(|l| l.parse().unwrap()).collect()
+fn parse_day10(input: &str) -> Vec<u64> {
+    input.lines().map(|l| l.parse().unwrap()).sorted().collect()
 }
 
 #[aoc(day10, part1)]
-fn solve_day10_part1(input: &[u16]) -> u16 {
-    let sorted = input.iter().sorted().collect::<Vec<_>>();
-    let results = sorted.iter().enumerate().fold(
-        HashMap::<u16, u16>::with_capacity(3),
+fn solve_day10_part1(input: &[u64]) -> u64 {
+    let results = input.iter().enumerate().fold(
+        HashMap::<u64, u64>::with_capacity(3),
         |mut acc, (idx, curr)| {
             let diff = if idx == 0 {
-                **curr
+                *curr
             } else {
-                **curr - *sorted[idx - 1]
+                *curr - input[idx - 1]
             };
             acc.insert(diff, *acc.get(&diff).unwrap_or(&0) + 1);
             acc
         },
     );
     results.get(&1).unwrap() * (results.get(&3).unwrap() + 1)
+}
+
+#[aoc(day10, part2)]
+fn solve_day10_part2(input: &[u64]) -> u64 {
+    let mut input = input.to_vec();
+    input.insert(0, 0);
+    input.push(input.last().unwrap() + 3);
+    visit_paths(&input, &mut HashMap::with_capacity(input.len()), 0)
+}
+
+fn visit_paths(input: &[u64], cache: &mut HashMap<usize, u64>, cursor: usize) -> u64 {
+    if let Some(cached) = cache.get(&cursor) {
+        return *cached;
+    }
+    if cursor == input.len() - 1 {
+        return 1;
+    }
+    if cursor >= input.len() {
+        return 0;
+    }
+    let current = input[cursor];
+    let mut visited_paths = 0;
+
+    let first = cursor + 1;
+    visited_paths += visit_paths(input, cache, first);
+
+    let second = cursor + 2;
+    if second < input.len() && input[second] - current <= 3 {
+        visited_paths += visit_paths(input, cache, second);
+    }
+
+    let third = cursor + 3;
+    if third < input.len() && input[third] - current <= 3 {
+        visited_paths += visit_paths(input, cache, third);
+    }
+
+    cache.insert(cursor, visited_paths);
+
+    visited_paths
 }
 
 #[cfg(test)]
@@ -81,5 +119,15 @@ mod tests {
     #[test]
     fn should_solve_part1_example2() {
         assert_eq!(solve_day10_part1(&parse_day10(EXAMPLE_2_INPUT)), 220);
+    }
+
+    #[test]
+    fn should_solve_part2_example1() {
+        assert_eq!(solve_day10_part2(&parse_day10(EXAMPLE_1_INPUT)), 8);
+    }
+
+    #[test]
+    fn should_solve_part2_example2() {
+        assert_eq!(solve_day10_part2(&parse_day10(EXAMPLE_2_INPUT)), 19208);
     }
 }
